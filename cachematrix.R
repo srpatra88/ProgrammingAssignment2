84e2f846af7eb9dac665232c6d37a15b69433219
Soumya Patra <spatra@bol.com>
2016-06-12 21:02:14 +0200

    implementing the inverse functions
    Change-Id: Id4a6b69ac3b0e4b0917b428cf82d53abb0269dd0

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  setmatrix <- function(matr) {
+    x <<- matr
+    inv <<- NULL
+  }
+  getmatrix <- function() x
+  setinverse <- function(invr) inv <<- invr
+  getinverse <- function() inv
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached reversed matrix")
+    return(inv)
+  }
+  matrx <- x$getmatrix()
+  inv <- solve(matrx, ...)
+  x$setinverse(inv)
+  inv
 }
